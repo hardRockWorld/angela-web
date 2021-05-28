7a8d6ec42f3cc09122312c92c275d6d49aeddbb0
Gourab Mahapatra <mgourab@hotmail.com>
2021-05-28 22:44:02 +0530

    - update fizzbuzz game with the while loop

--- js-practice/fizz_buzz_game.js
+++ js-practice/fizz_buzz_game.js
@@ -5,21 +5,22 @@ var count = 1;
 
 function fizzBuzz() {
   // write your code here
-  if (count % 3 === 0 && count % 5 === 0)  {
-    output.push('FizzBuzz');
-  } else if (count % 3 === 0) {
-    output.push('Fizz');
-  } else if (count % 5 === 0) {
-    output.push('Buzz');
-  } else {
-    output.push(count);
-  }
+  while (count < 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push('FizzBuzz');
+    } else if (count % 3 === 0) {
+      output.push('Fizz');
+    } else if (count % 5 === 0) {
+      output.push('Buzz');
+    } else {
+      output.push(count);
+    }
 
-  count++;
+    count++;
+  }
 
   console.log(output);
 }
 
-for (var i = 0; i < 30; i++) {
-  fizzBuzz();
-}
+// calling the function
+fizzBuzz();
